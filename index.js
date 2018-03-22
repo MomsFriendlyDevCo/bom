3a1911c2cbef8f6ab9e46640dc0f7f3d2c80153b
Matt Carter <m@ttcarter.com>
2018-03-22 10:50:51 +1000

    Added ability to fetch from local cache + form composite animation

--- index.js
+++ index.js
@@ -7,12 +7,18 @@ var events = require('events');
 var ftp = require('ftp');
 var fs = require('fs');
 var fspath = require('path');
+var glob = require('glob');
+var im = require('imagemagick');
 var mkdirp = require('mkdirp');
 var os = require('os');
 
 function BomRadar(options) {
 	var bom = this;
 
+	/**
+	* Settings to use for this BomRadar instance
+	* @var Object
+	*/
 	bom.settings = _.defaults(options, {
 		id: '032',
 		host: 'ftp.bom.gov.au',
@@ -22,21 +28,61 @@ function BomRadar(options) {
 		getThreads: 1,
 		fetch: {
 			frames: true,
-			backgrounds: {
-				background: true,
-				catchments: false,
-				locations: true,
-				rail: true,
-				range: false,
-				riverBasins: false,
-				roads: false,
-				topography: true,
-				waterWays: false,
-				wthrDistricts: false,
-			},
+			backgrounds: true,
+		},
+		backgrounds: {
+			background: true,
+			catchments: false,
+			locations: true,
+			rail: false,
+			range: false,
+			riverBasins: false,
+			roads: false,
+			topography: true,
+			waterWays: false,
+			wthrDistricts: false,
+		},
+		composite: {
+			format: 'gif',
+			method: 'path',
+			delay: 50,
+			arguments: [ // Any argument functions are evaluated as ({frames,backgrounds,settings})
+				// Set the animation delay + loop parameters
+				'-delay', b => b.settings.composite.delay,
+				'-loop', '0',
+
+				// Add background layers
+				b => _(b.backgrounds)
+					.pickBy((v, k) =>
+						v // Has a value
+						&& b.settings.backgrounds[k] // User has enabled the background
+					)
+					.map((v, k) => [
+						'-draw',
+						`image DstOver 0,0,0,0 '${fspath.join(b.settings.cachePath, b.backgrounds[k])}'`,
+					])
+					.sortBy(v => /\.background\.png/.test(v) ? `AAA${v}` : v) // Sort main background layer first
+					.value(),
+
+				// Clear the foreground for each frame
+				'-dispose', 'background',
+
+				// List the animation frames
+				b => b.frames.map(f => fspath.join(b.settings.cachePath, f)),
+
+				// Append the output file
+				b => fspath.join(b.settings.cachePath, `IDR${b.settings.id}.composite.${b.settings.composite.format}`),
+			],
 		},
 	});
 
+
+	/**
+	* Attempt to refresh BOM radar data from the FTP site
+	* @param {Object} [options] Additional options to use (overrides bom.settings)
+	* @param {function} cb Callback to call as (error, {backgrounds, frames})
+	* @returns {BomRadar} This chainable object
+	*/
 	bom.refresh = function(options, cb) {
 		// Argument mangling {{{
 		if (_.isFunction(options)) { // Called as (cb)
@@ -64,7 +110,7 @@ function BomRadar(options) {
 			// }}}
 			// Fetch backgrounds {{{
 			.then('backgrounds', function(next) {
-				if (!_.values(settings.fetch.backgrounds).some(i => i)) return next(); // All backgrounds disabled - then skip
+				if (!settings.fetch.backgrounds) return next(); // All backgrounds disabled - then skip
 				async()
 					.set('client', this.client)
 					// Change directory {{{
@@ -91,7 +137,7 @@ function BomRadar(options) {
 									var type = bits[1];
 
 									if (!type) return false;
-									if (!settings.fetch.backgrounds[type]) return false; // Dont fetch this type
+									if (!settings.backgrounds[type]) return false; // Dont fetch this type
 									return true;
 								})
 						);
@@ -237,6 +283,140 @@ function BomRadar(options) {
 					backgrounds: this.backgrounds,
 					frames: this.frames,
 				});
+			})
+			// }}}
+
+		return bom;
+	};
+
+
+	/**
+	* Retrieve the files we have stored locally on disk
+	* This operates the same as bom.refresh() but does not connect to the FTP - only using local data
+	* @param {Object} [options] Additional options to use (overrides bom.settings)
+	* @param {function} cb Callback to call as (error, {backgrounds, frames})
+	* @returns {BomRadar} This chainable object
+	*/
+	bom.cached = function(options, cb) {
+		// Argument mangling {{{
+		if (_.isFunction(options)) { // Called as (cb)
+			cb = options;
+			options = {};
+		}
+		// }}}
+
+		var settings = _.defaults(options, bom.settings);
+
+		async()
+			// Ensure cache directory exists {{{
+			.then(function(next) {
+				mkdirp(settings.cachePath, next);
+			})
+			// }}}
+			// Glob the directory {{{
+			.then('files', function(next) {
+				glob(fspath.join(settings.cachePath, '*.{gif,png}'), next);
+			})
+			// }}}
+			// Map files into basepaths only {{{
+			.then('files', function(next) {
+				next(null, this.files.map(f => fspath.basename(f)));
+			})
+			// }}}
+			// Break files into their correct sections {{{
+			.then('files', function(next) {
+				next(null, {
+					frames: this.files
+						.filter(f => f.startsWith(`IDR${settings.id}.T.`))
+						.filter(f => f.endsWith('.png')),
+					backgrounds: _(settings.backgrounds)
+						.mapValues((f, k) => _(this.files)
+							.filter(f => f.endsWith('.png'))
+							.filter(f => (new RegExp(`^IDR.*${k}\.png$`)).test(f))
+							.first()
+						)
+						.pickBy((v, k) => !!v)
+						.value(),
+				});
+			})
+			// }}}
+			// End {{{
+			.end(function(err) {
+				if (err) return cb(err);
+				cb(null, this.files);
+			});
+			// }}}
+
+		return bom;
+	};
+
+
+	/**
+	* Create a composite image based on the cached data from bom.refresh()
+	* This function creates a single GIF / Mp4 file which has a background + animated radar layers
+	* @param {Object} [options] Additional options to use (overrides bom.settings)
+	* @param {function} cb Callback to call as (error, buffer|path|stream)
+	* @returns {BomRadar} This chainable object
+	*/
+	bom.composite = function(options, cb) {
+		// Argument mangling {{{
+		if (_.isFunction(options)) { // Called as (cb)
+			cb = options;
+			options = {};
+		}
+		// }}}
+
+		var settings = _.defaults(options, bom.settings);
+
+		async()
+			// Fetch cached file list {{{
+			.then('files', function(next) {
+				bom.cached(next);
+			})
+			// }}}
+			// Compute ImageMagick arguments {{{
+			.then('arguments', function(next) {
+				next(null,
+					_(settings.composite.arguments)
+						.map(a =>
+							_.isFunction(a) ? a({
+								settings,
+								...this.files,
+							})
+							: a
+						)
+						.flattenDeep()
+						.value()
+				);
+			})
+			// }}}
+			// Run ImageMagick {{{
+			.then(function(next) {
+				if (debug.enabled) {
+					debug('ImageMagic> Run as -',
+						'convert '
+						+ this.arguments
+							.map(a => / /.test(a) ? `"${a}"` : a)
+							.join(' ')
+					);
+				}
+				im.convert(this.arguments, next);
+			})
+			// }}}
+			// Convert output into required type {{{
+			.then('result', function(next) {
+				switch (settings.composite.method) {
+					case 'path':
+						return cb(null, _.last(this.arguments));
+					default:
+						cb(`Unknown composite return method: "${settings.composite.method}"`);
+				}
+			})
+			// }}}
+			// End {{{
+			.end(function(err) {
+				if (err) return cb(err);
+				cb(this.result);
 			})
 			// }}}
 
--- test/test.js
+++ test/test.js
@@ -6,7 +6,7 @@ describe('bom.refresh()', function() {
 	var bom;
 	before('should setup a BOM instance', ()=> bom = new Bom({id: '032'})); // 032 = Sydney
 
-	it('should fetch radars for syndey', function(done) {
+	it('should fetch radars', function(done) {
 		this.timeout(30 * 1000);
 
 		bom.refresh(function(err, res) {
@@ -14,16 +14,62 @@ describe('bom.refresh()', function() {
 
 			expect(res).to.be.an.instanceOf(Object);
 			expect(res).to.have.property('backgrounds');
+			expect(res.backgrounds).to.be.an.instanceOf(Object);
+			expect(res.backgrounds).to.satisfy(i => _.values(i).every(l => _.isString(l)));
 
 			expect(res).to.have.property('frames');
 			expect(res.frames).to.have.length.above(5);
-			expect(res.frames).to.satisfy(()=> res.frames.every(l => _.isString(l)));
-
-			console.log('GOT', res);
+			expect(res.frames).to.satisfy(i => i.every(l => _.isString(l)));
 
 			done();
 		});
 
 	});
 
+});
+
+
+describe('bom.cached()', function() {
+	var bom;
+	before('should setup a BOM instance', ()=> bom = new Bom({id: '032'})); // 032 = Sydney
+
+	it('should fetch cached data', function(done) {
+		this.timeout(30 * 1000);
+
+		bom.cached(function(err, res) {
+			if (err) return done(err);
+
+			expect(res).to.be.an.instanceOf(Object);
+			expect(res).to.have.property('backgrounds');
+			expect(res.backgrounds).to.be.an.instanceOf(Object);
+			expect(res.backgrounds).to.satisfy(i => _.values(i).every(l => _.isString(l)));
+
+			expect(res).to.have.property('frames');
+			expect(res.frames).to.have.length.above(5);
+			expect(res.frames).to.satisfy(i => i.every(l => _.isString(l)));
+
+			done();
+		});
+
+	});
+});
+
+
+describe.only('bom.composite()', function() {
+	var bom;
+	before('should setup a BOM instance', ()=> bom = new Bom({id: '032'})); // 032 = Sydney
+
+	it('should fetch radars to a file path', function(done) {
+		this.timeout(30 * 1000);
+
+		bom.composite(function(err, res) {
+			if (err) return done(err);
+
+			expect(res).to.be.ok;
+			expect(res).to.match(/\/IDR.+\.gif$/);
+
+			done();
+		});
+
+	});
 });
